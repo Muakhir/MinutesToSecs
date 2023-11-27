490d9a5a584573b065152305f533d67a7c149378
Muakhir <almuakhirjones@gmail.com>
2023-11-27 10:00:21 +0200

    comments

--- script/code.js
+++ script/code.js
@@ -1,9 +1,11 @@
+// Create function
 function convert() {
 
     let Minutes = parseFloat(document.getElementById('minutes').value);
     let Seconds = (Minutes * 60);
     document.getElementById('seconds').value = Seconds.toFixed(2);
 }
+
 // function SecondsConvert(){
 //     let Seconds = parseFloat(document.getElementById('seconds').value);
 //     let Minutes = (Seconds / 60);
