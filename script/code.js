b8c49c364e3fc6ed4bbf105a1d86c370d5e9b326
Muakhir <almuakhirjones@gmail.com>
2023-11-27 08:31:08 +0200

    style

--- script/code.js
+++ script/code.js
@@ -4,8 +4,8 @@ function convert() {
     let Seconds = (Minutes * 60);
     document.getElementById('seconds').value = Seconds.toFixed(2);
 }
-function SecondsConvert(){
-    let Seconds = parseFloat(document.getElementById('seconds').value);
-    let Minutes = (Seconds / 60);
-    document.getElementById('minutes').value = Minutes.toFixed(2);
-}
+// function SecondsConvert(){
+//     let Seconds = parseFloat(document.getElementById('seconds').value);
+//     let Minutes = (Seconds / 60);
+//     document.getElementById('minutes').value = Minutes.toFixed(2);
+// }
